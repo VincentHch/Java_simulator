ea1ccd964ec02cd4666e3cdf99d708ed7b9355e3
Vincent Hachin <48289861+VincentHch@users.noreply.github.com>
2019-11-14 19:53:49 +0100

    Add files via upload

--- codeEtudiants/src/Boids/Boid.java
+++ codeEtudiants/src/Boids/Boid.java
@@ -1,5 +1,51 @@
 package Boids;
 
+import java.awt.*;
+import gui.*;
+
 public class Boid {
 
+	private Point pos;
+	private Point speed;
+	private Point accel;
+	
+	public Boid(Point pos, Point speed) {
+			this.pos = pos;
+			this.speed = speed;
+			this.accel = new Point(0,0);
+		}
+	
+	public Point get_steer(Boids boids) {
+		
+		Point steer = new Point();
+		steer += this.get_force1(boids);
+		steer += this.get_force2(boids);
+		steer += this.get_force3(boids);
+		return steer;
+	}
+	
+	public Point get_force1(Boids boids) {
+		return new Point();
+	}
+	
+	public Point get_force2(Boids boids) {
+		return new Point();
+	}
+	
+	public Point get_force3(Boids boids) {
+		return new Point();
+	}
+	
+	public void move(Boids boids) {
+		
+		Point steer = this.get_steer(boids);
+		this.speed.translate(steer.getX(), steer.getY());
+		this.pos.translate(speed.getX(), speed.getY());
+		
+	}
+	
+	public void draw(GUISimulator gui) {
+		
+		
+	}
 }
--- codeEtudiants/src/Boids/Boids.java
+++ codeEtudiants/src/Boids/Boids.java
@@ -6,9 +6,6 @@ import gui.*;
 public class Boids {
 	
 	private int n;
-	private Point[] pos;
-	private Point[] speed;
-	private Point[] accel;
 	
 	public Boids(int n) {
 		this.n = n;
